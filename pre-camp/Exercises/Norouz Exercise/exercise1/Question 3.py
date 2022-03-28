086125a0ca265468d5c20ca3a8e97a8a182a7a02
Moein Karimi <moeinkarimi.ed@gmail.com>
2022-03-29 03:00:02 +0430

    bugs have been fixed

--- pre-camp/Exercises/Norouz Exercise/exercise1/Question 3.py
+++ pre-camp/Exercises/Norouz Exercise/exercise1/Question 3.py
@@ -1,47 +1,44 @@
 string_list = list(input())
-counter = 0
+break_count = 0
+if string_list.count("?") == len(string_list):
+    print(False)
+    break_count +=1 
 for i in range(len(string_list)):
-    if string_list.count("?") == len(string_list) and string_list[i] != "?":
-        break
-    if string_list.count("?") == len(string_list) and string_list[i] == "?":
-        if string_list[i-1] == "0":
-            string_list.pop(i)
-            string_list.insert(i, "1")
-        elif string_list[i-1] == "1":
-            string_list.pop(i)
-            string_list.insert(i, "0")
-    if i == len(string_list)-1:
-        if string_list[i] == string_list[i-1]:
-            print(False)
+    if string_list[i] == "?":
+        j = i
+        counter = 0
+        while string_list[j] == "?":
             counter += 1
-            break
-    elif string_list[i] == "0" and i < len(string_list)-1:
-        if string_list[i+1] == "0":
-            print(False)
-            counter += 1
-            break
-        else:
-            continue
-    elif string_list[i] == "1" and i < len(string_list)-1:
-        if string_list[i+1] == "1":
-            print(False)
-            counter += 1
-            break
-        else:
-            continue
-    elif string_list[i] == "?":
+            j += 1
         if i == 0:
-            if string_list[i+1] != "?":
-                if string_list[i+1] == "0":
-                    string_list.pop(i)
-
-        elif string_list[i-1] == "0":
-            string_list.pop(i)
-            string_list.insert(i, "1")
-        elif string_list[i-1] == "1":
-            string_list.pop(i)
-            string_list.insert(i, "0")
-                    
-print(string_list)
-if counter == 0:
+            check = i
+            if counter % 2 == 0:
+                string_list[check] = string_list[j]
+                continue
+            elif counter % 2 != 0:
+                if string_list[j] == "0":
+                    string_list[check] = "1"
+                elif  string_list[j] == "1":
+                    string_list[check] = "0"
+        elif j == len(string_list):
+            check = i
+            while check < j:
+                if string_list[check-1] == "1":
+                    string_list[check] = "0"
+                elif string_list[check-1] == "0":
+                    string_list[check] = "1" 
+                check += 1
+        elif (counter % 2 != 0 and string_list[i-1] != string_list[j]) or (counter % 2 == 0 and string_list[i-1] == string_list[j]):
+            print(False)
+            break_count += 1
+        elif (counter % 2 == 0 and string_list[i-1] != string_list[j]) or (counter % 2 != 0 and string_list[i-1] == string_list[j]):
+            check = i
+            while check < j:
+                if string_list[check-1] == "1":
+                    string_list[check] = "0"
+                elif string_list[check-1] == "0":
+                    string_list[check] = "1"
+                check +=1
+if break_count == 0:
     print(True)
+    print(" ".join(string_list))
