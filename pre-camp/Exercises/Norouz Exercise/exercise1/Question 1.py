878e100a90159a51dc76cd3240d32466b4a44156
Moein Karimi <moeinkarimi.ed@gmail.com>
2022-03-28 17:46:01 +0430

    works pretty fine

---
+++ pre-camp/Exercises/Norouz Exercise/exercise1/Question 1.py
@@ -0,0 +1,19 @@
+while True:
+    rounds = int(input())
+    num_list = list(input())
+    counter = 0
+    for i in range(len(num_list)):
+        num_list.insert(i, int(num_list[i]))
+        num_list.pop(i+1)
+    orgin_list = num_list.copy()
+    for i in range(1, len(num_list)-1):
+        while num_list[i] > num_list[i-1] and num_list[i] > num_list[i+1]:
+            if num_list[i-1] != orgin_list[i-1]:
+                num_list[i-1] += 1
+            else:
+                num_list[i+1] += 1
+    for i in range(len(num_list)):
+        if num_list[i] != orgin_list[i]:
+            counter += 1                
+    print(counter)
+    print(num_list)
