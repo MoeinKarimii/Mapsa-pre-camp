e231c547704fb8d5a89a0294087e55a5f8eeed92
Moein Karimi <moeinkarimi.ed@gmail.com>
2022-04-01 18:10:04 +0430

    nothing critical

--- pre-camp/Exercises/Norouz Exercise/exercise 2/main.py
+++ pre-camp/Exercises/Norouz Exercise/exercise 2/main.py
@@ -4,10 +4,10 @@ from Account import Account
 
 user1 = Account("moien_karimi", "Llllll4555555", "09115480301", "moein@gmail.com")
 user2 = Site("url type")
-print(Site.register(user2, user1))
+print(user2.register(user1))
 print(user2.registered_users)
 # the first argumant must be password. username or email can be second or third
-print(Site.login(user2, Account.log_in("Llllll4555555", "moein@gmail.com")))
+print(user2.login(Account.log_in("Llllll4555555", "moein@gmail.com")))
 print(user2.active_users)
-print(Site.logout(user2, user1))
+print(user2.logout(user1))
 print(user2.active_users)
